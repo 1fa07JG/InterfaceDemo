c02cc8929f35e530d20af1c52cabf7b3b92c059c
1fa07JG <1fao7-gfi-2020@mail.de>
2021-11-12 14:30:22 +0100

    days final commit

--- src/main/java/com/example/interfacedemo/MenuController.java
+++ src/main/java/com/example/interfacedemo/MenuController.java
@@ -9,6 +9,8 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
 
 import java.io.*;
 import java.util.*;
@@ -28,6 +30,114 @@ public class MenuController {
     private ImageView picMonA;
 
     @FXML
+    private Button deleteTueA;
+
+    @FXML
+    private TextArea foodTueA;
+
+    @FXML
+    private TextField priceTueA;
+
+    @FXML
+    private ImageView picTueA;
+
+    @FXML
+    private Button deleteWedA;
+
+    @FXML
+    private TextArea foodWedA;
+
+    @FXML
+    private TextField priceWedA;
+
+    @FXML
+    private ImageView picWedA;
+
+    @FXML
+    private Button deleteThuA;
+
+    @FXML
+    private TextArea foodWThuA;
+
+    @FXML
+    private TextField priceThuA;
+
+    @FXML
+    private ImageView picThuA;
+
+    @FXML
+    private Button deleteFriA;
+
+    @FXML
+    private TextArea foodWFriA;
+
+    @FXML
+    private TextField priceFriA;
+
+    @FXML
+    private ImageView picFriA;
+
+    @FXML
+    private Button deleteMonB;
+
+    @FXML
+    private TextArea foodMonB;
+
+    @FXML
+    private TextField priceMonB;
+
+    @FXML
+    private ImageView picMonB;
+
+    @FXML
+    private Button deleteTueB;
+
+    @FXML
+    private TextArea foodTueB;
+
+    @FXML
+    private TextField priceTueB;
+
+    @FXML
+    private ImageView picTueB;
+
+    @FXML
+    private Button deleteWedB;
+
+    @FXML
+    private TextArea foodWedB;
+
+    @FXML
+    private TextField priceWedB;
+
+    @FXML
+    private ImageView picWedB;
+
+    @FXML
+    private Button deleteThuB;
+
+    @FXML
+    private TextArea foodWThuB;
+
+    @FXML
+    private TextField priceThuB;
+
+    @FXML
+    private ImageView picThuB;
+
+    @FXML
+    private Button deleteFriB;
+
+    @FXML
+    private TextArea foodWFriB;
+
+    @FXML
+    private TextField priceFriB;
+
+    @FXML
+    private ImageView picFriB;
+
+    @FXML
     private Button exit;
 
     String getFood(TextField food){
@@ -35,7 +145,7 @@ public class MenuController {
     }
 
     double getprice(TextField price){
-        return Double.valueOf(price.getText());
+        return Double.parseDouble(price.getText());
     }
 
     String getPicture(ImageView img) {
@@ -45,21 +155,24 @@ public class MenuController {
     @FXML
     void setPicture(MouseEvent event) throws IOException {
         // imageChosser einfügen
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Resource File");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif"));
+        Window mainStage = null;
+        File selectedFile = fileChooser.showOpenDialog(null);
+        Image image = new Image(selectedFile.getAbsolutePath());
+
         ImageView source = (ImageView) event.getSource();
         FileInputStream input = new FileInputStream("src/main/resources/images/cloudberry.jpg");
-        Image image = new Image(input);
+
         source.setImage(image);
     }
 
     @FXML
     void deleteMenu(ActionEvent event) {
-       /** TODO idee für die anwendbarkeit auf mehere Menüs anwenden.
-        Button source = (Button) event.getSource();
-        source.getId();
-        String kennung=source.getId().toString();
-        System.out.println(kennung);
-        Button food=("food"+kennung)**/
 
+        // Einfügen der Menüs und tage
        ArrayList<String> name = new ArrayList<>(Arrays.asList("deleteMonA", "deleteMonB", "deleteTueA", "deleteTueB"));
        ArrayList<TextArea> textAreas = new ArrayList<>(Arrays.asList(foodMonA, foodMonA, foodMonA));
        ArrayList<TextField> textFields=new ArrayList<>(Arrays.asList(priceMonA,priceMonA));
