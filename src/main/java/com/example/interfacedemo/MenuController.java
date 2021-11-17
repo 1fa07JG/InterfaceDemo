210f1f3b72050d525fc92bd358b5c66df17a0bd4
1fa07JG <1fao7-gfi-2020@mail.de>
2021-11-17 16:26:29 +0100

    final commit

--- src/main/java/com/example/interfacedemo/MenuController.java
+++ src/main/java/com/example/interfacedemo/MenuController.java
@@ -8,6 +8,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.InputMethodEvent;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 import javafx.stage.FileChooser;
 
 import java.io.File;
@@ -15,10 +16,12 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.time.Month;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Objects;
 import java.util.stream.IntStream;
+
 
 public class MenuController {
 
@@ -149,6 +152,16 @@ public class MenuController {
     private Label message;
 
     @FXML
+    private Label messagePrice;
+
+    @FXML
+    private Label kw;
+
+
+    @FXML
+    private GridPane grid;
+
+    @FXML
     private DatePicker date;
 
     public MenuController() {
@@ -158,18 +171,96 @@ public class MenuController {
     private Button verify;
 
     @FXML
-    void getWeek(InputMethodEvent event) {
+    void getWeek(ActionEvent event) {
 
         DatePicker datePicker= ((DatePicker) event.getSource());
         LocalDate date = datePicker.getValue();
-        System.out.println(date);
+        int umrechnung;
+
+        int year = date.getYear();
+        LocalDate firstDay= LocalDate.of(year, Month.JANUARY, 1);
+
+      //   LocalDate first=date.getYear();
+        switch (firstDay.getDayOfWeek()){
+            case MONDAY -> {
+                umrechnung=0;
+                break;
+            }
+            case TUESDAY -> {
+                umrechnung=1;
+                break;
+            }
+            case WEDNESDAY -> {
+                umrechnung=2;
+                break;
+            }
+            case THURSDAY -> {
+                umrechnung=3;
+                break;
+            }
+            case FRIDAY -> {
+                umrechnung=-3;
+                break;
+            }
+            case SATURDAY -> {
+                umrechnung=-2;
+                break;
+            }
+            case SUNDAY -> {
+                umrechnung=-1;
+                break;
+            }
+            default -> {
+                throw new IllegalStateException("Unexpected value: " + firstDay.getDayOfWeek());
+            }
+        }
+        int daysCW=(date.getDayOfYear())+umrechnung;
+        int calendarWeek=daysCW/7;
+        kw.setText("KW: "+calendarWeek);
     }
 
     @FXML
     void getWhite(KeyEvent event) {
         TextArea t= ((TextArea) event.getSource());
-        t.getStylesheets().remove("/style.css");
+        t.getStylesheets().remove("/failure.css");
                 System.out.println("called get white");
+    }
+    double toDouble(TextField text){
+        String s=text.getText();
+        return Double.parseDouble(s);
+    }
+
+    @FXML
+    void save(){
+        /*
+        Meal monA =new Meal(txtAreaFoodMonA.getText());
+        monA.setPrice(toDouble(priceMonA));
+        Meal monB =new Meal(txtAreaFoodMonB.getText());
+        monB.setPrice(toDouble(priceMonB));
+        Day mon=new Day(Monday);
+        Meal tueA =new Meal(txtAreaFoodTueA.getText());
+        tueA.setPrice(toDouble(priceTueA));
+        Meal tueB =new Meal(txtAreaFoodTueB.getText());
+        tueB.setPrice(toDouble(priceTueB));
+        Day tue=new Day(Tuesday);
+        Meal wedA =new Meal(txtAreaFoodWedA.getText());
+        wed.setPrice(toDouble(priceWedA));
+        Meal wedB =new Meal(txtAreaFoodWedB.getText());
+        wedB..setPrice(toDouble(priceWedB));
+        Day wed=new Day(Wendsday);
+        Meal thuA =new Meal(txtAreaFoodThuA.getText());
+        thuA.setPrice(toDouble(priceThuA));
+        Meal thuB =new Meal(txtAreaFoodThuB.getText());
+        thuB.setPrice(toDouble(priceThuB));
+        Day thu=new Day(Thursday);
+        Meal friA =new Meal(txtAreaFoodFriA.getText());
+        friA.setPrice(toDouble(priceFriA));
+        Meal friB =new Meal(txtAreaFoodFriB.getText());
+        friB.setPrice(toDouble(priceFriB));
+        Day fri=new Day(Friday);
+        Day []d=new Day[mon,tue,wed,thu,fri];
+    Week kw=new Week(days);*/
+        terminate();
     }
 
 
@@ -177,30 +268,54 @@ public class MenuController {
     void checkInput(ActionEvent event) {
         ArrayList<TextArea> textAreas = new ArrayList<>(Arrays.asList(txtAreaFoodMonA, txtAreaFoodMonB, txtAreaFoodTueA, txtAreaFoodTueB, txtAreaFoodWedA, txtAreaFoodWedB, txtAreaFoodThuA,
                 txtAreaFoodThuB, txtAreaFoodFriA, txtAreaFoodFriB));
+        ArrayList<TextField> textFields = new ArrayList<>(Arrays.asList(priceMonA, priceMonB, priceTueA, priceTueB, priceWedA, priceWedB,
+                priceThuA, priceThuB, priceFriA, priceFriB));
         int failure= 0;
+        int standardPrice=0;
         for (TextArea t : textAreas) {
 
 
             if (t.getText().equals("")) {
-                t.getStylesheets().add(("/style.css"));
+                t.getStylesheets().add(("/failure.css"));
 
                 System.out.println(t.getStyle());
                 failure++;
                 System.out.println("mindestens eine Speise hat keinen Namen ");
+            }else {
+                t.getStylesheets().remove("/failure.css");
             }
         }
-        IntStream.range(0, textAreas.size()).filter(i -> Objects.equals(textAreas.get(i).getText(), "")).forEach(i -> {
-            //failure.set(failure.get() + 1);
+        for (TextField f : textFields) {
 
-        });
+
+            if (f.getText().equals("")) {
+                f.getStylesheets().add(("/failure.css"));
+                f.setText("3.9");
+                standardPrice++;
+            }else {
+               f.getStylesheets().remove("/failure.css");
+            }
+        }
+
+
         switch ((failure)) {
             case 0 -> message.setText("");
             case 1 -> message.setText("Eine Speise hat keinen Namen ");
             default -> message.setText(failure + " Speisen haben keinen Namen ");
         }
+        switch ((standardPrice)) {
+            case 0 -> messagePrice.setText("");
+            case 1 -> messagePrice.setText("Für eine Speise wurde der Standard Preis fo 3,9 € festgelegt ");
+            default -> messagePrice.setText("Für "+standardPrice+" Speisen wurde der Preis fo 3,9 € festgelegt");
+        }
 
     }
-    //}
+
+    @FXML
+    public void initialize() {
+        grid.setStyle("-fx-background-color: #20e3e6;");
+    }
+
 
     String getFood(TextField food) {
         return food.getText();
